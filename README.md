8e7184fcbef7b767cf12cc313e1e94f8fbe0e7db
Avital Tamir <avital.betzefer@gmail.com>
2014-01-27 13:14:16 +0200

    Added default values, added usage instructions to README

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ angular-flowtype
 ================
 
 AngularJS directive for Flowtype.JS
+
+I love using Flowtype.JS and realized there's no out-of-the-box directive out there, pretty easy but thought I'd through it out there anyway.
+
+Usage:
+------
+
+ * Download and include angular-flowtype.js (and jQuery + Flowtype.JS, of course)
+
+        <script type='text/javascript' src='path/to/jquery.min.js'></script>
+        <script type='text/javascript' src='path/to/flowtype.min.js'></script>
+        <script type='text/javascript' src='path/to/angular-flowtype.js'></script>
+
+ * Add 'ngFlowtype' to your app's dependencies
+
+        angular.module('myApp', ['ngFlowtype']);
+
+ * Use the directive as an attribute, specify optional settings.
+
+        <div flowtype min-font="1" max-font="9999" font-ratio="35" line-ratio="1.45" minimum="1" maximum="9999"></div>
--- angular-flowtype.js
+++ angular-flowtype.js
@@ -2,18 +2,20 @@
 
 /* Directives */
 
-angular.module('ngFlowType', []).
+angular.module('ngFlowtype', []).
     directive('flowtype',function($compile)
               {
                   return {
                       restrict: 'A',
                       link: function(scope , element , attrs) {
-                          element.flowtype({
-                              minFont : attrs.minFont,
-                              maxFont : attrs.maxFont,
-                              fontRatio : attrs.fontRatio,
-                              lineRatio : attrs.lineRatio
-                          });
+			  var options = {};                        
+			  options.maximum = attrs.maximum || 9999;
+			  options.minimum = attrs.minimum || 1;
+                          options.minFont = attrs.minFont || 1;
+                          options.maxFont = attrs.maxFont || 9999;
+                          options.fontRatio = attrs.fontRatio || 35;
+                          options.lineRatio = attrs.lineRatio || 1.45;
+			  element.flowtype(options);
                       }
                   };
               });
