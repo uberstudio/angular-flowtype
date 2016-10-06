0e162cdcc48b3e1798dccbae2cee8e0fe95b1b6d
Ihor <Badabum@users.noreply.github.com>
2016-10-06 12:21:06 +0300

    refactoring according to styleguide

--- angular-flowtype.js
+++ angular-flowtype.js
@@ -1,21 +1,30 @@
-'use strict';
+(function(){
+  'use strict';
 
-/* Directives */
+  /* Directives */
 
-angular.module('ngFlowtype', [])
-.directive('flowtype', ['$compile', function($compile) {
-  return {
-    restrict: 'A',
-    link: function(scope , element , attrs) {
-      var options = {};
-      options.maximum = attrs.maximum || 9999;
-      options.minimum = attrs.minimum || 1;
-      options.minFont = attrs.minFont || 1;
-      options.maxFont = attrs.maxFont || 9999;
-      options.fontRatio = attrs.fontRatio || 35;
-      options.lineRatio = attrs.lineRatio || 1.45;
-      element.flowtype(options);
-    }
-  };
-}]);
+ angular.module('ngFlowtype', []);
+  
+ angular.module('ngFlowtype').directive('flowtype', flowtype);
+  
+  flowtype.$inject = []
+  function flowtype() {
+    return {
+      restrict: 'A',
+      link: link
+    };
+    function link(scope , element , attrs) {
+        var options = {};
+        options.maximum = attrs.maximum || 9999;
+        options.minimum = attrs.minimum || 1;
+        options.minFont = attrs.minFont || 1;
+        options.maxFont = attrs.maxFont || 9999;
+        options.fontRatio = attrs.fontRatio || 35;
+        options.lineRatio = attrs.lineRatio || 1.45;
+        element.flowtype(options);
+      }
+ }
+})();
+
+
 
